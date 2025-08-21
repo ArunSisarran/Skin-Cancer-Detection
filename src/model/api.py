31f0ae31f9de8152dcc573dc78295b366c98353c
Arun Sisarran <arunsisarran@gmail.com>
2025-08-20 20:04:50 -0400

    updated CORS for production

--- src/model/api.py
+++ src/model/api.py
@@ -24,13 +24,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Local
-        "http://localhost:5173",  # Vite 
         "https://*.vercel.app",   # Vercel 
-        "https://*.netlify.app",  # Netlify 
-        "*"
     ],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
